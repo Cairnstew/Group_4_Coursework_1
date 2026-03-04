1ecb8aea9a0c7f8bf85202bd5c930b0db44c49ef
Cairnstew <sean.cairnsst@gmail.com>
2026-03-04 11:51:14 -0600

    Install Python 3 alongside OpenJDK 21 in setup-jenkins.sh

--- scripts/setup-jenkins.sh
+++ scripts/setup-jenkins.sh
@@ -27,7 +27,7 @@ if java -version &>/dev/null; then
 else
   info "Installing Java 17 and dependencies..."
   apt-get update -qq
-  apt-get install -y fontconfig openjdk-21-jre
+  apt-get install -y fontconfig openjdk-21-jre python3
   info "Java installed: $(java -version 2>&1 | head -1)"
 fi
 
