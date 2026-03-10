de2e82f6ac9a059a265541c0126cab71977774b4
Cairnstew <sean.cairnsst@gmail.com>
2026-03-10 17:00:17 +0000

    chore(python): Rollback python.

--- Dec2Hex.py
+++ Dec2Hex.py
@@ -1,12 +1,5 @@
 import sys
-
-# Simulate Change Comment
-
 def decimal_to_hex(decimal_value):
-    if decimal_value == 0:
-        print("Hexadecimal representation is: 0")
-        return "0"
-    
     hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
     hexadecimal = ""
     num = decimal_value
@@ -17,7 +10,6 @@ def decimal_to_hex(decimal_value):
         num //= 16
     print(f"Hexadecimal representation is: {hexadecimal}")
     return hexadecimal
-
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         try:
