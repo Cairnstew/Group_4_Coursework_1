a91955a301329262f56eba8cd4978c5eda0dfdc5
Cairnstew <sean.cairnsst@gmail.com>
2026-03-10 17:32:05 +0000

    feat(evidence): Add outcomes/evidence folder

---
+++ old-Dec2Hex.py
@@ -0,0 +1,16 @@
+import sys
+def decimal_to_hex(decimal_value):
+    hex_chars = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
+    hexadecimal = ""
+    num = decimal_value
+    print(f"Converting the Decimal Value {num} to Hex...")
+    while num != 0:
+        rem = num % 16
+        hexadecimal = hex_chars[rem] + hexadecimal
+        num //= 16
+    print(f"Hexadecimal representation is: {hexadecimal}")
+    return hexadecimal
+
+if __name__ == "__main__":
+    decimal_value = int(sys.argv[1])  # crashes if no arg or non-integer
+    decimal_to_hex(decimal_value)
